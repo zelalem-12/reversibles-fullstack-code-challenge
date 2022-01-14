9ea2c129ef77211b215b9fbd14f4517e249ed274
zelalem A. Enyew <zelalem.antigegn12@gmail.com>
2022-01-15 01:22:42 +0300

    allowing CORS polcy

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
+// Allowing CORS policy
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    next();
+});
+
 /**
  * Mounting globbal middleware to process every request.
  */
