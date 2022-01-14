cdd5b27e73fe5ac34e45eeeb83c1fc5b90d56d62
zelalem A. Enyew <zelalem.antigegn12@gmail.com>
2022-01-15 01:23:18 +0300

    api rout

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const { getReversibleNumbers } = require('../controllers')
 
 module.exports = function (app) {
-  app.get("/", getReversibleNumbers);
+  app.get("/api", getReversibleNumbers);
 
   app.use(function (req, res) {
     res.status(404).send("Resource not found!");
