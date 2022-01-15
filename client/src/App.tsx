17879734d30712c3a4901d0a3efa959a492b34c1
zelalem A. Enyew <zelalem.antigegn12@gmail.com>
2022-01-15 04:49:07 +0300

    adding loading feature

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,14 @@ import './App.css';
 const App: FC = (): ReactElement => {
   const [number, setNumber] = useState<string>('');
   const [reversibleNumbers, setReversibleNumbers] = useState<Array<number>>([])
+  const [loading, setLoading] = useState(false)
 
   const fetchReversibleNumbers = async (num: number): Promise<void> => {
+    setLoading(true);
     const response: Response = await fetch(`http://localhost:5000/api?number=${num}`);
-    const numbers = await response.json()
+    const numbers = await response.json();
+    setLoading(false)
+
     setReversibleNumbers(numbers)
   }
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -34,13 +38,15 @@ const App: FC = (): ReactElement => {
         <h3>The list of <span style={{ color: 'green' }}>{reversibleNumbers.length}</span> reversible numbers up to  <span style={{ color: 'red' }}>{number} </span></h3> : null}
       <hr />
       {
-        parseInt(number) ?
-          reversibleNumbers.length ? (
-            <div className='nembers_container'>
-              <NumbersList numbers={reversibleNumbers} />
-            </div>) :
-            <div className='empity_container'> <h3> Not found</h3></div> :
-          <div className='empity_container'> <h2>Please Enter any positive integer to see reversible numbers  up to it</h2></div>
+        !loading ?
+          parseInt(number) ?
+            reversibleNumbers.length ? (
+              <div className='nembers_container'>
+                <NumbersList numbers={reversibleNumbers} />
+              </div>) :
+              <div className='empity_container'> <h3> Not found</h3></div> :
+            <div className='empity_container'> <h2>Please Enter any positive integer to see reversible numbers  up to it</h2></div> :
+          <div className='empity_container'> <h3> ...Loading</h3></div>
       }
     </div >
   );
